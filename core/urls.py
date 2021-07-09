0a37de9f30a159c3a103dca5dddf7d470a99db5a
Tom Byers <tmbyers1102@gmail.com>
2021-07-09 16:09:56 -0700

    ooops should not have added the admin to core urls

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from django.urls import path
 from .views import (
     FileUploadView,
@@ -15,14 +14,14 @@ from .views import (
 app_name = 'core'
 
 urlpatterns = [
-    path('admin/', admin.site.urls, name="admin"),
     path('demo/', FileUploadView.as_view(), name='file-upload-demo'),
     path('email/', UserEmailView.as_view(), name='email'),
     path('change-email/', ChangeEmailView.as_view(), name='change-email'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('billing/', UserDetailsView.as_view(), name='billing'),
     path('subscribe/', SubscribeView.as_view(), name='subscribe'),
-    path('cancel-subscription/', CancelSubscription.as_view(), name='cancel-subscription'),
+    path('cancel-subscription/', CancelSubscription.as_view(),
+         name='cancel-subscription'),
     path('upload/', ImageRecognitionView.as_view(), name='image-recognition'),
     path('api-key/', APIKeyView.as_view(), name='api-key'),
 ]
